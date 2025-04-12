913a0346d7754d2af9283dc0b3d5e448d3160bf3
amir-mc <amirfooladi93@gmail.com>
2025-04-12 13:46:59 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+   <main>
+      <div>
+        <h1 className='sans-serif'>
+          my 3d pro
+        </h1>
+      </div>
+   </main>
+  )
+}
+
+export default App
