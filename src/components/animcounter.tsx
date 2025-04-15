e22d2d7a5dc21489d791ea8c508b09675c77790c
amir-mc <amirfooladi93@gmail.com>
2025-04-15 16:35:59 +0000

    update

--- src/components/animcounter.tsx
+++ src/components/animcounter.tsx
@@ -6,8 +6,8 @@ const AnimatedCounter = () => {
         <div id="counter" className="padding-x-lg xl:mt-0 mt-32 ">
             <div className="max-auto grid-4-cols">
                 {counterItems.map((items)=>(
-                    <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
-                        <div className="counter-number text-white text-5xl font-bold mb-2" key={items.label}>
+                    <div key={items.label} className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+                        <div className="counter-number text-white text-5xl font-bold mb-2" >
                          <CountUp suffix= {items.suffix} end={items.value}/>  
                         </div>
                         <div className="text-white-50 text-lg">
--- src/section/showcase.tsx
+++ src/section/showcase.tsx
@@ -1,20 +1,32 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import { useRef } from "react";
 
 const Showcase = () => {
-    const showsection=()=>{
-        const sectionref = useRef(null)
+gsap.registerPlugin(ScrollTrigger)
+    
+        
+        const sectionref =useRef(null)
         const project1Ref = useRef(null)
         const project2Ref = useRef(null)
         const project3Ref = useRef(null)
         // ERROR IN SHOWCASE 
-        
-    }
+        useGSAP(()=>{
+            gsap.fromTo(
+                sectionref.current,
+                {opacity:0},
+                {opacity:1,duration:1.5}
+            )
+        })
+    
     return ( 
-        <section  id="work" className="app-showcase">
+        <section ref={sectionref}  id="work" className="app-showcase">
             <div className="w-full">
+                <div className="showcaselayout">
                 {/* LEFT SIDE */}
-                <div  className="first-project-wrapper">
-                    <div className="image-wrapper">
+                <div ref={project1Ref}  className="first-project-wrapper">
+                    <div  className="image-wrapper">
                         <img src="/images/project1.png" alt=""/>
                     </div>
                     <div className="text-content">
@@ -24,27 +36,30 @@ const Showcase = () => {
                         <p className="text-white-50 md:text-xl">
                             Aliqua velit duis qui amet consectetur reprehenderit laborum ea tempor in culpa culpa voluptate excepteur.
                         </p>
+                    </div>
+                    </div>
                         {/* RIGHT SIDE */}
                         <div className="project-list-wrapper overflow-hidden">
-                            <div className="project">
+                            <div className="project" ref={project2Ref}>
                                 <div className="image-wrapper bg-[#ffefdb]">
                                     <img src="images/project2.png" alt=""/>
                                 </div>
                                 <h2>site</h2>
                             </div>
-                            <div className="project">
+                            <div ref={project3Ref} className="project">
                                 <div className="image-wrapper bg-[#ffe7eb]">
                                     <img src="images/project3.png" alt=""/>
                                 </div>
                                 <h2>site</h2>
                             </div>
                         </div>
+                    
                     </div>
-                </div>
             </div>
 
         </section>
      );
+    
 }
  
 export default Showcase;
