5a8d50b6c645df89ca8b3c748f05aaf0b93706ec
amir-mc <amirfooladi93@gmail.com>
2025-07-13 15:17:45 +0000

    add buy me coffee link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { navLinks } from "../data/worditem";
 
+
 const Navbar = () => {
     const [scrolled ,setscroll]=useState(false)
     
@@ -33,13 +34,32 @@ const Navbar = () => {
                         }
                      </ul>
                 </nav>
-                <a href="https://blogin-two.vercel.app/" className="contant-btn group ">
-                    <div className="inner">
-                        <span>
-                            My Personal Blog
-                        </span>
+               <div className="flex items-center space-x-4">
+                    {/* Personal Blog Link (Purple) */}
+                    <a 
+                        href="https://blogin-two.vercel.app/" 
+                        className="group relative inline-block"
+                    >
+                        <div className="inner px-4 py-2 bg-white rounded-lg transition-all duration-300
+                                        group-hover:-translate-y-0.5
+                                        group-hover:shadow-[0_8px_16px_-3px_rgba(147,51,234,0.4)]">
+                        <span className="text-gray-800 font-medium">My Personal Blog |</span>
+                        </div>
+                    </a>
+
+                    {/* Buy Me Coffee Link (Blue) */}
+                    <a 
+                        href="https://buymcoff.vercel.app/" 
+                        className="group relative inline-block"
+                    >
+                        <div className="inner px-4 py-2 bg-white rounded-lg transition-all duration-300
+                                        group-hover:-translate-y-0.5
+                                        group-hover:shadow-[0_8px_16px_-3px_rgba(59,130,246,0.4)]">
+                        <span className="text-gray-800 font-medium">Buy Me a Coffee</span>
+                        </div>
+                    </a>
                     </div>
-                </a>
+                
             </div>
         </header>
      );
